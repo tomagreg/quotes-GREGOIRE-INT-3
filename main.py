b0ed705ec599e96d1f55226b488a2570763e6d2f
Dioubot <133760650+Dioubot@users.noreply.github.com>
2023-05-23 12:52:08 +0200

    interger remplace by string

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from functions import *
 
 
-
 def menu():
     print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. Display quotes")
-    print("3. Add quote")
-    print("4. Exit")
-
+    print("random : Random quote")
+    print("display : Display quotes")
+    print("add : Add a new quote")
+    print("exit : Exit the program")
 
 
 def main():
@@ -16,16 +14,16 @@ def main():
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
+        choice = input(">> ")
 
-        if choice == "1":
+        if choice == "random":
             print_quote(random_quote(quotes))
-        elif choice == "2":  # handling display_count()
+        elif choice == "display":
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
-        elif choice == "3":
-            add_quote(quotes,"quotes.txt")
-        elif choice == "4":
+        elif choice == "add":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "exit":
             print("Good bye...")
             break
         else:
